daba50a90c53aa5aae1f42e00c407e5e5523d621
Khashiev <nxashiev@gmail.com>
2023-05-04 21:04:54 +0300

    add all flags

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,57 +1,121 @@
+#include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-  if (argc == 2) {
-    FILE* ptr;
-    char ch;
-    ptr = fopen(argv[1], "r");
+#include "CatFlags.h"
 
-    if (NULL == ptr) {
-      printf("file can't be opened \n");
+typedef struct {
+  bool numberNonBlank;
+  bool markEndl;
+  bool numberAll;
+  bool squeeze;
+  bool tab;
+  bool printNonPrintable;
+} Flags;
+
+Flags catReadFlags(int argc, char **argv) {
+  struct option longOptions[] = {{"number-nonblank", 0, NULL, 'b'},
+                                 {"number", 0, NULL, 'n'},
+                                 {"squeeze-blank", 0, NULL, 's'},
+                                 {0, 0, 0, 0}};
+
+  Flags flags = {0};
+  int currentFlag = getopt_long(argc, argv, "benstvET", longOptions, NULL);
+
+  while (currentFlag != -1) {
+    switch (currentFlag) {
+      case 'b':
+        flags.numberNonBlank = true;
+        break;
+      case 'e':
+        flags.markEndl = true;
+        flags.printNonPrintable = true;
+        break;
+      case 'n':
+        flags.numberAll = true;
+        break;
+      case 's':
+        flags.squeeze = true;
+        break;
+      case 't':
+        flags.tab = true;
+        flags.printNonPrintable = true;
+        break;
+      case 'v':
+        flags.printNonPrintable = true;
+        break;
+      case 'T':
+        flags.tab = true;
+        break;
+      case 'E':
+        flags.markEndl = true;
     }
 
-    do {
-      ch = fgetc(ptr);
-
-      if ((int)ch < 0 || (int)ch > 126) continue;
-
-      printf("%c", ch);
-    } while (ch != EOF);
-
-    fclose(ptr);
-
-  } else if (argc == 3) {
-    FILE* ptr;
-    char ch;
-    ptr = fopen(argv[2], "r");
-
-    if (NULL == ptr) {
-      printf("file can't be opened \n");
-    }
-
-    int counter = 1;
-    printf("%6d  ", counter);
-    counter++;
-
-    do {
-      ch = fgetc(ptr);
-
-      if ((int)ch < 0 || (int)ch > 126) continue;
-
-      printf("%c", ch);
-
-      if (ch == '\n') {
-        printf("%6d  ", counter);
-        counter++;
-      }
-    } while (ch != EOF);
-
-    fclose(ptr);
-  } else {
-    printf("0");
+    currentFlag = getopt_long(argc, argv, "benstvET", longOptions, NULL);
   }
+  return flags;
+}
 
-  return 0;
+void CatFile(FILE *file, Flags flags, const char *table[static 256]) {
+  int c = 0;
+  int last = '\n';
+  bool sq = false;
+  int line = 0;
+
+  while (fread(&c, 1, 1, file) > 0) {
+    if (last == '\n') {
+      if (flags.squeeze && c == '\n') {
+        if (sq) continue;
+        sq = true;
+      } else {
+        sq = false;
+      }
+
+      if (flags.numberNonBlank) {
+        if (c != '\n') {
+          printf("%6i\t", ++line);
+        }
+      }
+
+      else if (flags.numberAll)
+        printf("%6i\t", ++line);
+    }
+
+    if (!*table[c])
+      printf("%c", '\0');
+    else
+      printf("%s", table[c]);
+    last = c;
+  }
+}
+
+void Cat(int argc, char *argv[], Flags flags, const char *table[static 256]) {
+  for (char **filename = &argv[1], **end = &argv[argc]; filename != end;
+       ++filename) {
+    if (**filename == '-') continue;
+
+    FILE *file = fopen(*filename, "rb");
+    if (errno) {
+      fprintf(stderr, "%s", argv[0]);
+      perror(*filename);
+      continue;
+    }
+    CatFile(file, flags, table);
+    fclose(file);
+  }
+}
+
+int main(int argc, char **argv) {
+  Flags flags = catReadFlags(argc, argv);
+  const char *table[256];
+
+  CatSetTable(table);
+
+  if (flags.markEndl) CatSetEndl(table);
+  if (flags.tab) CatSetTab(table);
+  if (flags.printNonPrintable) CatSetNonPrintable(table);
+
+  Cat(argc, argv, flags, table);
 }
